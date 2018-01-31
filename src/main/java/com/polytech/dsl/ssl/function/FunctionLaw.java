a57ae58998d5e6c285f7ac8c0c66286bcef208c8
danial <azswad20@gmail.com>
2018-01-31 11:10:02 +0100

    Mathematical laws

---
+++ src/main/java/com/polytech/dsl/ssl/function/FunctionLaw.java
@@ -0,0 +1,13 @@
+package com.polytech.dsl.ssl.function;
+
+public class FunctionLaw implements SensorLaw{
+    @Override
+    public void input() {
+
+    }
+
+    @Override
+    public void output() {
+
+    }
+}
---
+++ src/main/java/com/polytech/dsl/ssl/function/SensorLaw.java
@@ -0,0 +1,8 @@
+package com.polytech.dsl.ssl.function;
+
+public interface SensorLaw {
+
+    void input();
+
+    void output();
+}
