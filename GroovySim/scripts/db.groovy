d08ab2baaaeb4cc0d1a48ee8844b86a935d725a9
danial <azswad20@gmail.com>
2018-02-13 20:48:45 +0100

    timestamp for sensors update

--- GroovySim/scripts/db.groovy
+++ GroovySim/scripts/db.groovy
@@ -1,9 +1,9 @@
 simulation "simulation" timerange "now", "13-02-2018 23:59:00" offset "SECOND", 1
 
-sensor "sampah" randomlaw 10, 40
+sensor "temo" randomlaw 10, 40
 sensor "light" randomlaw 20, 30
 
-generateSet "sampah", 100
+generateSet "temp", 100
 generateSet "light", 500
 
 outputDB "SSL"
