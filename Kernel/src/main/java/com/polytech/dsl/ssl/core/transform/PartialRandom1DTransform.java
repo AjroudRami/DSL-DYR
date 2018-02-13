b3202f2f56be79b92a4df232acf959d3d1d1e4e9
Rami <ajroud.rami@hotmail.fr>
2018-02-13 23:38:44 +0100

    Add partial random transform for numeric sensors

---
+++ Kernel/src/main/java/com/polytech/dsl/ssl/core/transform/PartialRandom1DTransform.java
@@ -0,0 +1,34 @@
+package com.polytech.dsl.ssl.core.transform;
+
+import com.polytech.dsl.ssl.source.SensorMeasure;
+
+import java.util.Random;
+
+/**
+ * This class implements a variant of the Random1DTransformer which will transform
+ * or not, sensor values, following a uniform law.
+ * Only an average of "probability" % of the dataset will be modified.
+ */
+public class PartialRandom1DTransform extends Random1DTransform {
+
+    private double amplitude;
+    private double probability;
+
+    public PartialRandom1DTransform(double probability, double amplitude) {
+        this.amplitude = amplitude;
+        this.probability = probability;
+    }
+
+    @Override
+    public SensorMeasure transform(SensorMeasure measure) {
+        if (mustApplyTransform()) {
+            super.transform(measure, this.amplitude);
+        }
+        return measure;
+    }
+
+    private boolean mustApplyTransform() {
+        Random random = new Random();
+        return random.nextDouble() < probability;
+    }
+}
--- Kernel/src/main/java/com/polytech/dsl/ssl/core/transform/Random1DTransform.java
+++ Kernel/src/main/java/com/polytech/dsl/ssl/core/transform/Random1DTransform.java
@@ -6,24 +6,43 @@ import java.util.Arrays;
 import java.util.Optional;
 import java.util.Random;
 
+/**
+ * This class implements a transform function that will change ALL values
+ * in a 1 dimensional numeric dataset.
+ * It will apply for each value a gain or a loss of +- an amplitude.
+ * The calculated variation follows a pseudo-random uniform law
+ */
 public class Random1DTransform implements SensorMeasureTransform {
 
     private double amplitude;
 
+    /**
+     * Create a random transformer that will apply to all sensor measure in a set
+     *
+     * @param amplitude the maximum amplitude of the variation.
+     *                  Eg: if amplitude is 10.0 then the max positive variation is 10 / 2
+     */
     public Random1DTransform(double amplitude) {
         this.amplitude = amplitude;
     }
 
+    protected Random1DTransform() {
+    }
+
     @Override
     public SensorMeasure transform(SensorMeasure measure) {
+        return transform(measure, this.amplitude);
+    }
+
+    protected SensorMeasure transform(SensorMeasure measure, double amplitude) {
         SensorMeasure res = new SensorMeasure(measure.time());
         Arrays.stream(measure.getLabels()).forEach(
                 label -> {
                     Random random = new Random();
                     Optional<Integer> opt = measure.getInt(label);
                     if (opt.isPresent()) {
-                        double value = measure.getInt(label).get();
-                        double noiseValue = value + random.nextDouble() * amplitude;
+                        double value = opt.get();
+                        double noiseValue = value + random.nextDouble() * (amplitude / 2);
                         res.putValue(label, noiseValue);
                     }
                 }
