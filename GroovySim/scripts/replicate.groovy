320e8b9ecb63ece641a610c132e6f7f0709d8191
Yasin EROGLU <yasin.eroglu@etu.unice.fr>
2018-02-14 00:11:20 +0100

    Fix date and grammar

--- GroovySim/scripts/replicate.groovy
+++ GroovySim/scripts/replicate.groovy
@@ -1,4 +1,4 @@
-simulation "simulation" timerange "now", "13-02-2018 23:59:00" offset 1,"SECOND"
+simulation "simulation" timerange "now", "14-02-2018 23:59:00" offset 1,"SECOND"
 
 sensor "replicated" fromcsv "/home/user/DSL-DYR/GroovySim/src/test/resources/toreplicate.csv"
 
